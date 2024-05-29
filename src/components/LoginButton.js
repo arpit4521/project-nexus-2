67989101f645e9a290a13d8a934a3b0ca9285ce9
arpit4521 <arpitgupta4521@gmail.com>
2024-05-30 00:57:25 +0530

    first commit

---
+++ src/components/LoginButton.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const LoginButton = () => {
+    const {loginWithRedirect} = useAuth0();
+
+    return (
+        <button onClick={() => loginWithRedirect}>
+            Log In
+        </button>
+    )
+}
